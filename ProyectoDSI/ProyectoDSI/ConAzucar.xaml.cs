227cdd83668b96f245876ae4f069789e4c6b4abd
sarais02 <sarais02@ucm.es>
2022-03-29 16:57:39 +0200

    Timer

--- ProyectoDSI/ProyectoDSI/ConAzucar.xaml.cs
+++ ProyectoDSI/ProyectoDSI/ConAzucar.xaml.cs
@@ -22,14 +22,44 @@ namespace ProyectoDSI
     /// </summary>
     public sealed partial class ConAzucar : Page
     {
+        private DispatcherTimer timer;
+        private int minutes = 3;
+        private int seconds = 0;
         public ConAzucar()
         {
             this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+
+            timer.Tick += timer_Tick;
+            CountDown.Text = "0" + minutes.ToString() + ":0" + seconds.ToString();
+            timer.Start();
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Pausa));
+        }
+
+        void timer_Tick(object sender, object e)
+        {
+            if (minutes > 0)
+            {
+                if (seconds == 0) { seconds = 59; minutes--; }
+                else seconds--;
+
+                if(seconds<10) CountDown.Text = "0" + minutes.ToString() + ":0" + seconds.ToString();
+                else CountDown.Text = "0" + minutes.ToString() + ":" +  seconds.ToString();
+            }
+            else if(minutes==0 && seconds <= 59)
+            {
+                if (seconds == 0) { timer.Stop(); }
+                else seconds--;
+
+                if (seconds < 10) CountDown.Text = "0" + minutes.ToString() + ":0" + seconds.ToString();
+                else CountDown.Text = "0" + minutes.ToString() + ":" + seconds.ToString();
+            }
         }
     }
 }
