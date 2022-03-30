d7af4f2680cb7eeeca2d26a2a029c046e6ab3490
Javier <javcomas@ucm.es>
2022-03-30 13:13:43 +0200

    Paneneles de informacion y estado inicial(IN PROGRESS))

--- ProyectoDSI/ProyectoDSI/SinAzucar.xaml.cs
+++ ProyectoDSI/ProyectoDSI/SinAzucar.xaml.cs
@@ -20,7 +20,9 @@ namespace ProyectoDSI
 {
     struct PanelFicha{
         public Ficha ficha_;
-        public int num_;         
+        public int num_;
+        public string rango_;
+        public string numFichas_;
     }
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
@@ -28,6 +30,7 @@ namespace ProyectoDSI
     public sealed partial class SinAzucar : Page
     {
         List<PanelFicha> listPanelFichas;
+        bool[,] Tablero;
         public SinAzucar()
         {
             this.InitializeComponent();
@@ -35,48 +38,11 @@ namespace ProyectoDSI
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Pausa));
+            Frame.Navigate(typeof(Pausa));         
         }
-        protected override void OnNavigatedTo(NavigationEventArgs e){
-            listPanelFichas = new List<PanelFicha>();
-            PanelFicha panelFicha;
-            //regaliz
-            panelFicha.ficha_ = new Ficha(-1, "Regaliz", -1, -1);
-            panelFicha.num_ = 1;
-            listPanelFichas.Add(panelFicha);
-            //baston
-            panelFicha.ficha_ = new Ficha(-1, "Baston", -1, -1);
-            panelFicha.num_ = 2;
-            listPanelFichas.Add(panelFicha);
-            //dedos
-            panelFicha.ficha_ = new Ficha(-1, "Dedo", -1, -1);
-            panelFicha.num_ = 3;
-            listPanelFichas.Add(panelFicha);
-            //huevo
-            panelFicha.ficha_ = new Ficha(-1, "Huevo", -1, -1);
-            panelFicha.num_ = 4;
-            listPanelFichas.Add(panelFicha);
-            //sandia
-            panelFicha.ficha_ = new Ficha(-1, "Sandia", -1, -1);
-            panelFicha.num_ = 4;
-            listPanelFichas.Add(panelFicha);
-            //cocacola
-            panelFicha.ficha_ = new Ficha(-1, "Cocacola", -1, -1);
-            panelFicha.num_ = 8;
-            listPanelFichas.Add(panelFicha);
-            //chicle sandia
-            panelFicha.ficha_ = new Ficha(-1, "BombaSandia", -1, -1);
-            panelFicha.num_ = 1;
-            listPanelFichas.Add(panelFicha);
-            //petazetas
-            panelFicha.ficha_ = new Ficha(-1, "Petazeta", -1, -1);
-            panelFicha.num_ = 6;
-            listPanelFichas.Add(panelFicha);
-            //fresa
-            panelFicha.ficha_ = new Ficha(-1, "Fresa", -1, -1);
-            panelFicha.num_ = 1;
-            listPanelFichas.Add(panelFicha);
-          
+        protected override void OnNavigatedTo(NavigationEventArgs e){            
+            CrearLista();
+            inicializateTablero();
         }
 
         private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
@@ -84,6 +50,74 @@ namespace ProyectoDSI
             if(e.Key==VirtualKey.Escape) 
                 Frame.Navigate(typeof(Pausa));
         }
-      
+        private void CrearLista(){
+            listPanelFichas = new List<PanelFicha>();
+            PanelFicha panelFicha;
+            //regaliz
+            panelFicha.ficha_ = new Ficha(-1, "Regaliz", -1, -1);
+            panelFicha.num_ = 1;
+            panelFicha.rango_ = "7";
+            panelFicha.numFichas_ = "x1";
+            listPanelFichas.Add(panelFicha);
+            //baston
+            panelFicha.ficha_ = new Ficha(-1, "Baston", -1, -1);
+            panelFicha.num_ = 2;
+            panelFicha.rango_ = "6";
+            panelFicha.numFichas_ = "x2";
+            listPanelFichas.Add(panelFicha);
+            //dedos
+            panelFicha.ficha_ = new Ficha(-1, "Dedo", -1, -1);
+            panelFicha.num_ = 3;
+            panelFicha.rango_ = "5";
+            panelFicha.numFichas_ = "x3";
+            listPanelFichas.Add(panelFicha);
+            //huevo
+            panelFicha.ficha_ = new Ficha(-1, "Huevo", -1, -1);
+            panelFicha.num_ = 4;
+            panelFicha.rango_ = "4";
+            panelFicha.numFichas_ = "x4";
+            listPanelFichas.Add(panelFicha);
+            //sandia
+            panelFicha.ficha_ = new Ficha(-1, "Sandia", -1, -1);
+            panelFicha.num_ = 4;
+            panelFicha.rango_ = "3";
+            panelFicha.numFichas_ = "x4";
+            listPanelFichas.Add(panelFicha);
+            //cocacola
+            panelFicha.ficha_ = new Ficha(-1, "Cocacola", -1, -1);
+            panelFicha.num_ = 8;
+            panelFicha.rango_ = "2";
+            panelFicha.numFichas_ = "x8";
+            listPanelFichas.Add(panelFicha);
+            //chicle sandia
+            panelFicha.ficha_ = new Ficha(-1, "BombaSandia", -1, -1);
+            panelFicha.num_ = 1;
+            panelFicha.rango_ = "1";
+            panelFicha.numFichas_ = "x1";
+            listPanelFichas.Add(panelFicha);
+            //petazetas
+            panelFicha.ficha_ = new Ficha(-1, "Petazeta", -1, -1);
+            panelFicha.num_ = 6;
+            panelFicha.rango_ = "B";
+            panelFicha.numFichas_ = "x6";
+            listPanelFichas.Add(panelFicha);
+            //fresa
+            panelFicha.ficha_ = new Ficha(-1, "Fresa", -1, -1);
+            panelFicha.num_ = 1;
+            panelFicha.rango_ = "F";
+            panelFicha.numFichas_ = "x1";
+            listPanelFichas.Add(panelFicha);
+        }
+        private void inicializateTablero()
+        {
+            Tablero = new bool[10, 10];          
+            for (int i = 0; i < Tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < Tablero.GetLength(1); j++)
+                {
+                    Tablero[i, j] = false;
+                }
+            }
+        }
     }
 }
